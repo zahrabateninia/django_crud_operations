2ba99731cac9fa8133b88040dde48a771aa286e9
zahrabateninia <zahrabatenin@gmail.com>
2024-06-01 13:28:52 +0330

    Create urls.py inside crudTasks and add its url path to urlpattern in taskProj/urls.py

--- taskProj/crudTasks/views.py
+++ taskProj/crudTasks/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Task
 
-# Create your views here.
+
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+
+def task_create(request):
+    return 
+
+
+def task_update(request, pk):
+    return 
+
+
+def task_delete(request, pk):
+    return 
