a4cef2d19f2cdb2182f0ded042260cf207ec50d8
zahrabateninia <zahrabatenin@gmail.com>
2024-06-01 12:36:04 +0330

    Create model task.

---
+++ taskProj/crudTasks/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CrudtasksConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'crudTasks'
---
+++ taskProj/crudTasks/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    due_date = models.DateTimeField()
+    is_completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
