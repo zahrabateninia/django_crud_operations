42daec0ffb12907db878a5a9376e708fb5b092d2
zahrabateninia <zahrabatenin@gmail.com>
2024-06-08 19:52:12 +0330

    Update the urls.py to provide correct paths.

--- taskProj/crudTasks/urls.py
+++ taskProj/crudTasks/urls.py
@@ -1,15 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
 from django.urls import path
+from .import views
 from .views import task_list, task_create, task_update, task_delete
-
 
 urlpatterns = [
     # Map the root URL of the tasks/ path to the task_list view, which will display the list of tasks.
-    path('', task_list, name='task-list'),
-    # Map the tasks/create/ path to the task_create view, which will display a form to create a new task.
-    path('create/', task_create, name='task-create'),
-    # Map the tasks/update/<int:pk>/ path to the task_update view, which will display a form to update an existing task.
-    path('update/<int:pk>/', task_update, name='task-update'),
-    # It displays a confirmation page to delete a task.
-    path('delete/<int:pk>/', task_delete, name='task-delete'),
+    path('', views.task_list, name='task_list'),
+    path('create/', views.task_create, name='task_create'),
+    path('update/<int:pk>/', views.task_update, name='task_update'),
+    path('delete/<int:pk>/', views.task_delete, name='task_delete'),
 ]
 
