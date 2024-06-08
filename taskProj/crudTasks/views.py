54dd5163d6891288fdf41f358efe2f5f88030a95
zahrabateninia <zahrabatenin@gmail.com>
2024-06-08 19:42:36 +0330

    Improve the user experience by integrating Ajax to handle form submissions and deletions.
    it's allowing for a more dynamic and interactive user interface without requiring full page reloads.

--- taskProj/crudTasks/views.py
+++ taskProj/crudTasks/views.py
@@ -1,3 +1,5 @@
+from django.http import JsonResponse
+from django.urls import reverse
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Task
 from .forms import TaskForm
@@ -11,30 +13,31 @@ def task_create(request):
     if request.method == 'POST':
         form = TaskForm(request.POST)
         if form.is_valid():
-            form.save() # save the new task to the db
-            return redirect('task-list')
+            new_task = form.save()
+            return JsonResponse({'success': True, 'redirect': reverse('task_list')})
+        else:
+            return JsonResponse({'success': False, 'errors': form.errors}, status=400)
     else:
-        form = TaskForm() # It creates an empty form instance if the request method is not POST.
-    return render(request, 'tasks/task_form.html', {'form': form})
-
+        form = TaskForm()
+        return render(request, 'tasks/task_form.html', {'form': form})
+    
 def task_update(request, pk):
-    # Retrieve the task with the given primary key (pk), or return a 404 error if it doesn't exist.
     task = get_object_or_404(Task, pk=pk)
     if request.method == 'POST':
-        # Bind form data to the existing task instance.
         form = TaskForm(request.POST, instance=task)
         if form.is_valid():
-            form.save()
-            return redirect('task-list')
+            updated_task = form.save()
+            return JsonResponse({'success': True, 'task': {'id': updated_task.id, 'title': updated_task.title, 'description': updated_task.description}})
+        else:
+            return JsonResponse({'success': False, 'errors': form.errors}, status=400)
     else:
-        #  Create a form pre-filled with the task's data if the request method is not POST.
         form = TaskForm(instance=task)
-    return render(request, 'tasks/task_form.html', {'form': form})
+        return render(request, 'tasks/task_form.html', {'form': form})
 
 def task_delete(request, pk):
     task = get_object_or_404(Task, pk=pk)
     if request.method == 'POST':
         task.delete()
-        return redirect('task-list')
-    #  Render a confirmation page before deleting the task.
-    return render(request, 'tasks/task_confirm_delete.html', {'task': task})
+        return JsonResponse({'success': True})
+    else:
+        return render(request, 'tasks/task_confirm_delete.html', {'task': task})
